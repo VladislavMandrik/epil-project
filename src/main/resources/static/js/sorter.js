4a1b39199defc453daaaa3e33368d7e2ac5d0df6
vm <ladvdrikman@gmail.com>
2023-04-11 22:23:50 +0300

    v 1.0.3

--- src/main/resources/static/js/sorter.js
+++ src/main/resources/static/js/sorter.js
@@ -1,19 +1,30 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-    const getSort = ({ target }) => {
+    const getSort = ({target}) => {
         const order = (target.dataset.order = -(target.dataset.order || -1));
         const index = [...target.parentNode.cells].indexOf(target);
-        const collator = new Intl.Collator(['en', 'ru'], { numeric: true });
-        const comparator = (index, order) => (a, b) => order * collator.compare(
-            a.children[index].innerHTML,
-            b.children[index].innerHTML
-        );
+        const collator = new Intl.Collator(['en', 'ru'], {numeric: true});
+        const tableRowComparator = (index, order) => {
+            return (rowA, rowB) => {
+                const cellA = rowA.children[index].innerHTML;
+                const cellB = rowB.children[index].innerHTML;
 
-        for(const tBody of target.closest('table').tBodies)
-            tBody.append(...[...tBody.rows].sort(comparator(index, order)));
+                if (index === 2) {
+                    const cellADateArray = cellA.split('.');
+                    const cellBDateArray = cellB.split('.');
 
-        for(const cell of target.parentNode.cells)
-            cell.classList.toggle('sorted', cell === target);
+// new Date(year, monthIndex, day)
+                    return order * (new Date(cellADateArray[2], cellADateArray[1] - 1, cellADateArray[0])) -
+                        (new Date(cellBDateArray[2], cellBDateArray[1] - 1, cellBDateArray[0]));
+                }
+
+                return order * collator.compare(cellA, cellB)
+            };
+        };
+
+        for (const tBody of target.closest('table').tBodies) tBody.append(...[...tBody.rows].sort(tableRowComparator(index, order)));
+
+        for (const cell of target.parentNode.cells) cell.classList.toggle('sorted', cell === target);
     };
 
     document.querySelectorAll('.table_sort thead').forEach(tableTH => tableTH.addEventListener('click', () => getSort(event)));
